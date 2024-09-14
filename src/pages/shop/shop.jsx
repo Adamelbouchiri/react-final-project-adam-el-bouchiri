0ac4a27204d4bd9351d0174c2426d2314011cc1a
Adamelbouchiri <matrakmtk@gmail.com>
2024-09-14 14:12:24 +0200

    addign-shop-header

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export const Shop = () => {
   return (
-    <div></div>
+    <div>
+      <div className="flex blog bg-cover bg-center text-white font-extrabold text-5xl justify-center items-center h-[220px] bg-blue-50">
+        <div>Shop</div>
+      </div>
+    </div>
   );
 };
